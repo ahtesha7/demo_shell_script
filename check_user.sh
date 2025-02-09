61caf72089b4aed574c8b686b8e14b3ade25451e
Ubuntu <ubuntu@ip-172-31-43-79.eu-west-1.compute.internal>
2025-02-09 04:18:10 +0000

    added shell scripts

---
+++ check_user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+<<info
+This shell script checks if user exists
+
+info
+
+read -p "Enter the username you wish to check" username
+
+count=$(cat /etc/passwd | grep $username | wc | awk '{print $1}')
+
+echo "$count"
---
+++ hello_chatur.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Me : Good Morning!"
+
+echo "Me : Hello"
+
+echo "Imran : Good Morning!!"
+
+echo "Aftab : Assalamualaikum"
+
+mkdir -p FolderinShellScript
+
+echo "Aftab : Shell script k andar folder bana diya"
+
+echo "Durkshan : Good Work Bhaii!"
+
+echo "Shell script k andar hi ek text file bana li" > shelltextfile.txt
